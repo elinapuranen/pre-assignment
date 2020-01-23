e0c2afc0e81a220ed8f3c22ea31772219c0711ec
elina <elina.puranen@outlook.com>
2020-01-24 00:15:14 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function parseFormat(data) {
+  var
+    category,
+    output = {};
+  
+  data.split('\n').forEach(function(line) {
+      var item = line.split(':');
+      if (item[0].match(/Package/gi)) {
+          category = item[1].trim()
+          output[category] = []
+      } else {
+          var name = item[0].trim()
+          output[category].push({
+              [name]: item[1].trim()
+          })
+      }
+      return output;
+  })
+}
+
+var finalOutput = []
+
+var parseString = parseFormat('../status.real')
+
+finalOutput.push(parseString)
+
+console.log(JSON.stringify(finalOutput));
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
